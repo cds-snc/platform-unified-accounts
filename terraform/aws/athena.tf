43b99c7d48f3a23c4ba395ed3b65aab82edb267b
Pat Heard <patrick.heard@cds-snc.ca>
2026-03-11 14:08:22 -0400

    feat: add WAF logs and Athena queries (#49)
    Setup WAF logs to the CBS satellite bucket and create the Athena
    workgroups and tables that will allow us to query them.

---
+++ terraform/aws/athena.tf
@@ -0,0 +1,38 @@
+#
+# Create Athena queries to view the WAF and load balancer access logs
+#
+module "athena_access_logs" {
+  source = "github.com/cds-snc/terraform-modules//athena_access_logs?ref=v10.11.0"
+
+  athena_bucket_name = module.athena_bucket.s3_bucket_id
+
+  lb_access_queries_create   = true
+  lb_access_log_bucket_name  = var.cbs_satellite_bucket_name
+  waf_access_queries_create  = true
+  waf_access_log_bucket_name = var.cbs_satellite_bucket_name
+
+  billing_tag_value = var.billing_tag_value
+}
+
+#
+# Hold the Athena data
+#
+module "athena_bucket" {
+  source            = "github.com/cds-snc/terraform-modules//S3?ref=v10.11.0"
+  bucket_name       = "${var.product_name}-${var.env}-athena"
+  billing_tag_value = var.billing_tag_value
+
+  versioning = {
+    enabled = true
+  }
+
+  lifecycle_rule = [
+    {
+      id      = "expire-objects"
+      enabled = true
+      expiration = {
+        days = 30
+      }
+    },
+  ]
+}
--- terraform/aws/locals.tf
+++ terraform/aws/locals.tf
@@ -1,6 +1,7 @@
 locals {
-  vpc_az_count      = 2
-  protocol_versions = toset(["HTTP1", "HTTP2"])
+  cbs_satellite_bucket_arn = "arn:aws:s3:::${var.cbs_satellite_bucket_name}"
+  vpc_az_count             = 2
+  protocol_versions        = toset(["HTTP1", "HTTP2"])
   common_tags = {
     Terraform  = "true"
     CostCentre = var.billing_tag_value
