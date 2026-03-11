ed8673e4a89376166caf0ab3b2e78af3201348bf
Pat Heard <patrick.heard@cds-snc.ca>
2026-03-11 11:51:16 -0400

    fix: past-Pat was one step ahead it would seem

--- terraform/aws/waf.tf
+++ terraform/aws/waf.tf
@@ -467,27 +467,3 @@ data "aws_iam_policy_document" "idp_waf_logs" {
     ]
   }
 }
-
-#
-# AWS Shield Advanced
-#
-resource "aws_shield_subscription" "idp" {
-  auto_renew = "ENABLED"
-}
-
-resource "aws_shield_protection" "idp_alb" {
-  name         = "idp-alb"
-  resource_arn = aws_lb.idp.arn
-  tags         = local.common_tags
-}
-
-resource "aws_shield_protection" "idp_route53" {
-  name         = "idp-route53"
-  resource_arn = aws_route53_zone.idp.arn
-  tags         = local.common_tags
-}
-
-resource "aws_shield_application_layer_automatic_response" "idp_alb" {
-  resource_arn = aws_lb.idp.arn
-  action       = "BLOCK"
-}
