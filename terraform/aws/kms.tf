0aed1ad99f6922cd446168a56c83a9d2349562a3
Pat Heard <patrick.heard@cds-snc.ca>
2026-06-27 17:42:24 +0000

    feat: turn on DNSSEC for Staging
    Enable DNSSEC for the `auth.cdssandbox.xyz` Staging domain.
    Once `canada.ca` has this enabled, Production can be turned on
    as well.

--- terraform/aws/kms.tf
+++ terraform/aws/kms.tf
@@ -72,3 +72,88 @@ data "aws_iam_policy_document" "kms_cloudwatch" {
     }
   }
 }
+
+#
+# KMS key for Route53 DNSSEC signing
+#
+resource "aws_kms_key" "dnssec" {
+  provider = aws.us-east-1
+
+  count                    = var.env == "staging" ? 1 : 0
+  description              = "KMS key for Route53 DNSSEC signing - auth.cdssandbox.xyz"
+  customer_master_key_spec = "ECC_NIST_P256"
+  key_usage                = "SIGN_VERIFY"
+  deletion_window_in_days  = 7
+  enable_key_rotation      = false
+  policy                   = data.aws_iam_policy_document.kms_dnssec.json
+
+  tags = local.core_tags
+}
+
+data "aws_iam_policy_document" "kms_dnssec" {
+  statement {
+    sid       = "Enable IAM root permissions"
+    effect    = "Allow"
+    actions   = ["kms:*"]
+    resources = ["*"]
+
+    principals {
+      type        = "AWS"
+      identifiers = ["arn:aws:iam::${var.account_id}:root"]
+    }
+  }
+
+  statement {
+    sid    = "Allow Route53 DNSSEC to use the key"
+    effect = "Allow"
+    actions = [
+      "kms:DescribeKey",
+      "kms:GetPublicKey",
+      "kms:Sign",
+    ]
+    resources = ["*"]
+
+    principals {
+      type        = "Service"
+      identifiers = ["dnssec-route53.amazonaws.com"]
+    }
+
+    condition {
+      test     = "StringEquals"
+      variable = "aws:SourceAccount"
+      values   = [var.account_id]
+    }
+  }
+
+  statement {
+    sid       = "Allow Route53 DNSSEC to create grants"
+    effect    = "Allow"
+    actions   = ["kms:CreateGrant"]
+    resources = ["*"]
+
+    principals {
+      type        = "Service"
+      identifiers = ["dnssec-route53.amazonaws.com"]
+    }
+
+    condition {
+      test     = "Bool"
+      variable = "kms:GrantIsForAWSResource"
+      values   = ["true"]
+    }
+
+    condition {
+      test     = "StringEquals"
+      variable = "aws:SourceAccount"
+      values   = [var.account_id]
+    }
+  }
+}
+
+resource "aws_kms_alias" "dnssec" {
+  provider = aws.us-east-1
+
+  count         = var.env == "staging" ? 1 : 0
+  name          = "alias/auth-cdssandbox-xyz-dnssec"
+  target_key_id = aws_kms_key.dnssec[0].key_id
+}
