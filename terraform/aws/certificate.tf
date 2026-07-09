1fd196567374a4a8119d38046e71f7c279965eb9
Pat Heard <patrick.heard@cds-snc.ca>
2026-07-09 13:27:05 +0000

    feat: add root CA for ECS Service Connect
    Add a root CA and certificate for use by ECS Service Connect's
    encryption-in-transit configuration.

--- terraform/aws/certificate.tf
+++ terraform/aws/certificate.tf
@@ -1,3 +1,6 @@
+#
+# IdP primary certificate
+#
 resource "aws_acm_certificate" "idp" {
   domain_name               = var.domain
   subject_alternative_names = ["*.${var.domain}"]
@@ -31,4 +34,51 @@ resource "aws_route53_record" "idp_validation" {
 resource "aws_acm_certificate_validation" "idp" {
   certificate_arn         = aws_acm_certificate.idp.arn
   validation_record_fqdns = [for record in aws_route53_record.idp_validation : record.fqdn]
+}
+
+#
+# ECS Service Connect: encryption-in-transit between ECS services
+#
+resource "aws_acmpca_certificate_authority" "ecs_service_connect" {
+  type                            = "ROOT"
+  usage_mode                      = "SHORT_LIVED_CERTIFICATE"
+  permanent_deletion_time_in_days = 7
+
+  certificate_authority_configuration {
+    key_algorithm     = "EC_secp384r1"
+    signing_algorithm = "SHA384WITHECDSA"
+
+    subject {
+      common_name         = "ecs.idp"
+      organization        = "CDS"
+      organizational_unit = "Platform"
+    }
+  }
+
+  # Revocation not supported for short-lived certificates
+  revocation_configuration {
+    crl_configuration {
+      enabled = false
+    }
+  }
+
+  tags = local.common_tags
+}
+
+resource "aws_acmpca_certificate" "ecs_service_connect" {
+  certificate_authority_arn   = aws_acmpca_certificate_authority.ecs_service_connect.arn
+  certificate_signing_request = aws_acmpca_certificate_authority.ecs_service_connect.certificate_signing_request
+  signing_algorithm           = "SHA384WITHECDSA"
+  template_arn                = "arn:aws:acm-pca:::template/RootCACertificate/V1"
+
+  validity {
+    type  = "DAYS"
+    value = 7
+  }
+}
+
+resource "aws_acmpca_certificate_authority_certificate" "ecs_service_connect" {
+  certificate_authority_arn = aws_acmpca_certificate_authority.ecs_service_connect.arn
+  certificate               = aws_acmpca_certificate.ecs_service_connect.certificate
+  certificate_chain         = aws_acmpca_certificate.ecs_service_connect.certificate_chain
 }
