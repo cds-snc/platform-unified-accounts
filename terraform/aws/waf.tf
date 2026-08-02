4e1ae7ba588670f916ae03a8ee44900fa0468519
Pat Heard <patrick.heard@cds-snc.ca>
2026-08-02 10:20:44 -0400

    fix: WAF deny-by-default
    Update the WAF to block all requests by default and then use
    a valid host rule to allow requests that match the expected
    `Host` header.

--- terraform/aws/waf.tf
+++ terraform/aws/waf.tf
@@ -17,7 +17,42 @@ resource "aws_wafv2_web_acl" "idp" {
   scope = "REGIONAL"
 
   default_action {
-    allow {}
+    block {}
+  }
+
+  rule {
+    name     = "ValidHost"
+    priority = 1
+
+    action {
+      allow {}
+    }
+
+    statement {
+      byte_match_statement {
+        field_to_match {
+          single_header {
+            name = "host"
+          }
+        }
+        text_transformation {
+          priority = 1
+          type     = "COMPRESS_WHITE_SPACE"
+        }
+        text_transformation {
+          priority = 2
+          type     = "LOWERCASE"
+        }
+        positional_constraint = "EXACTLY"
+        search_string         = var.domain
+      }
+    }
+
+    visibility_config {
+      cloudwatch_metrics_enabled = true
+      metric_name                = "ValidHost"
+      sampled_requests_enabled   = true
+    }
   }
 
   rule {
@@ -89,45 +124,6 @@ resource "aws_wafv2_web_acl" "idp" {
     visibility_config {
       cloudwatch_metrics_enabled = true
       metric_name                = "BlockLargeRequests"
-      sampled_requests_enabled   = true
-    }
-  }
-
-  rule {
-    name     = "InvalidHost"
-    priority = 20
-
-    action {
-      block {}
-    }
-
-    statement {
-      not_statement {
-        statement {
-          byte_match_statement {
-            field_to_match {
-              single_header {
-                name = "host"
-              }
-            }
-            text_transformation {
-              priority = 1
-              type     = "COMPRESS_WHITE_SPACE"
-            }
-            text_transformation {
-              priority = 2
-              type     = "LOWERCASE"
-            }
-            positional_constraint = "EXACTLY"
-            search_string         = var.domain
-          }
-        }
-      }
-    }
-
-    visibility_config {
-      cloudwatch_metrics_enabled = true
-      metric_name                = "InvalidHost"
       sampled_requests_enabled   = true
     }
   }
