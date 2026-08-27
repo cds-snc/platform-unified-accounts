48284876efa454b5245dea48f11775bad7d2bd90
Pat Heard <patrick.heard@cds-snc.ca>
2026-08-27 18:30:42 +0000

    chore: remove the system API user
    System user can be removed now that the new custom domain
    has been added.
    
    This also updates the Lambda functions to use this new custom domain.

--- terraform/aws/ecs.tf
+++ terraform/aws/ecs.tf
@@ -145,7 +145,7 @@ module "idp_ecs" {
 
   # Task definition
   container_image                     = "${aws_ecr_repository.repo["idp"].repository_url}:latest"
-  container_command                   = ["start-from-init", "--masterkeyFromEnv", "--tlsMode", "external", "--config", "/app/config.yaml", "--steps", "/app/steps.yaml"]
+  container_command                   = ["start", "--masterkeyFromEnv", "--tlsMode", "external", "--config", "/app/config.yaml"]
   container_host_port                 = 8080
   container_port                      = 8080
   container_environment               = local.idp_container_env
--- terraform/aws/vpc.tf
+++ terraform/aws/vpc.tf
@@ -475,16 +475,6 @@ resource "aws_security_group_rule" "idp_cleanup_users_egress_internal_lb" {
   source_security_group_id = aws_security_group.idp_internal_lb.id
 }
 
-resource "aws_security_group_rule" "idp_cleanup_users_egress_internet" {
-  description       = "Egress from idp cleanup users to internet"
-  type              = "egress"
-  to_port           = 443
-  from_port         = 443
-  protocol          = "tcp"
-  security_group_id = aws_security_group.idp_cleanup_users.id
-  cidr_blocks       = ["0.0.0.0/0"]
-}
-
 resource "aws_security_group_rule" "idp_cleanup_users_egress_s3" {
   description       = "Egress from idp cleanup users to S3"
   type              = "egress"
@@ -521,16 +511,6 @@ resource "aws_security_group_rule" "idp_event_exporter_egress_internal_lb" {
   protocol                 = "tcp"
   security_group_id        = aws_security_group.idp_event_exporter.id
   source_security_group_id = aws_security_group.idp_internal_lb.id
-}
-
-resource "aws_security_group_rule" "idp_event_exporter_egress_internet" {
-  description       = "Egress from idp event exporter to internet"
-  type              = "egress"
-  to_port           = 443
-  from_port         = 443
-  protocol          = "tcp"
-  security_group_id = aws_security_group.idp_event_exporter.id
-  cidr_blocks       = ["0.0.0.0/0"]
 }
 
 resource "aws_security_group_rule" "idp_event_exporter_egress_s3" {
