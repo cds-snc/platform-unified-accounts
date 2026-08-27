558cc1fa7bc63cc2f045eabd014ec7ddc1707539
Omar Tehsin <omartehsin@gmail.com>
2026-08-27 16:39:54 -0400

    move to the private zone

--- terraform/aws/route53.tf
+++ terraform/aws/route53.tf
@@ -95,7 +95,7 @@ resource "aws_route53_hosted_zone_dnssec" "idp" {
 # Internal ALB subdomain
 #
 resource "aws_route53_record" "idp_internal_A" {
-  zone_id = aws_route53_zone.idp.zone_id
+  zone_id = aws_route53_zone.idp_internal.zone_id
   name    = "internal.${var.domain}"
   type    = "A"
 
