87d65bcb0a06d00697d676c5fc80309fdf21b477
Pat Heard <patrick.heard@cds-snc.ca>
2026-08-28 14:14:56 +0000

    fix: ALB must have 2 subnet associations
    Revert the subnet association change to the ALB and update the VPN
    to associate with both subnets.

--- terraform/aws/locals.tf
+++ terraform/aws/locals.tf
@@ -5,9 +5,6 @@ locals {
   vpc_endpoints_interface  = toset(["ecr.api", "ecr.dkr", "logs", "monitoring", "rds", "ssm"])
   vpc_endpoints_gateway    = toset(["s3"])
 
-  # Production associates the VPN with all private subnets (improves redundancy), but non-production only associates with the first private subnet (save costs)
-  vpn_private_subnet_association_ids = var.env == "production" ? module.idp_vpc.private_subnet_ids : slice(module.idp_vpc.private_subnet_ids, 0, 1)
-
   common_tags = {
     Terraform  = "true"
     CostCentre = var.billing_tag_value
