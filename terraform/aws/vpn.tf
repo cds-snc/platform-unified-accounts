614bd77f91404a7bb9af8dfa28472a64686ca227
Pat Heard <patrick.heard@cds-snc.ca>
2026-08-28 14:01:07 +0000

    fix: internal ALB and VPN subnet associations
    Make sure the VPN is always associated with the same private subnets
    as the internal ALB.

--- terraform/aws/vpn.tf
+++ terraform/aws/vpn.tf
@@ -13,7 +13,7 @@ module "client_vpn" {
   vpc_id              = module.idp_vpc.vpc_id
   vpc_cidr_block      = module.idp_vpc.cidr_block
   subnet_cidr_blocks  = module.idp_vpc.private_subnet_cidr_blocks
-  subnet_ids          = [tolist(module.idp_vpc.private_subnet_ids)[0]]
+  subnet_ids          = local.vpn_private_subnet_association_ids
   acm_certificate_arn = aws_acm_certificate.client_vpn.arn
 
   # Only create a self-service portal in prod  
