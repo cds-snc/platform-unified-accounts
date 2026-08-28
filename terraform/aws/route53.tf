9df2f76a63462e2df65907a04674e57fe5629589
Omar Tehsin <omartehsin@gmail.com>
2026-08-28 08:46:50 -0400

    terraform fmt

--- terraform/aws/route53.tf
+++ terraform/aws/route53.tf
@@ -108,7 +108,7 @@ resource "aws_route53_record" "idp_internal_A" {
 
 # Private zone for internal ALB
 resource "aws_route53_zone" "idp_internal" {
-  name    = "internal.${var.domain}"
+  name = "internal.${var.domain}"
 
   vpc {
     vpc_id = module.idp_vpc.vpc_id
