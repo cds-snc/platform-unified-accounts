8633a5106b049b3e1d13d27de87cb17b29fac407
Omar Tehsin <omartehsin@gmail.com>
2026-09-03 11:03:43 -0400

    removed lambda schedule module

--- terraform/aws/idp_cleanup_users.tf
+++ terraform/aws/idp_cleanup_users.tf
@@ -1,37 +1,6 @@
 /*
  * Lambda function to cleanup users
  */
-module "idp_cleanup_users_lambda" {
-  source = "github.com/cds-snc/terraform-modules//lambda_schedule?ref=v11.4.5"
-
-  lambda_name                = "idp-cleanup-users"
-  lambda_schedule_expression = "cron(0 0 * * ? *)" # Run every day at midnight UTC
-  lambda_timeout             = "60"
-  lambda_architectures       = ["arm64"]
-  lambda_ecr_arn             = aws_ecr_repository.repo["idp-cleanup-users"].arn
-  lambda_image_uri           = aws_ecr_repository.repo["idp-cleanup-users"].repository_url
-
-  lambda_policies = [
-    data.aws_iam_policy_document.idp_cleanup_users_get_ssm_parameters.json,
-    data.aws_iam_policy_document.idp_cleanup_users_sqs.json
-  ]
-
-  lambda_environment_variables = {
-    INACTIVE_DAYS                = 30
-    ZITADEL_PRIVATE_KEY_SSM_PATH = aws_ssm_parameter.idp_cleanup_users_key_json.name
-    ZITADEL_URL                  = "idp.${var.domain}"
-    DRY_RUN                      = "false"
-  }
-
-  lambda_vpc_config = {
-    subnet_ids         = module.idp_vpc.private_subnet_ids
-    security_group_ids = [aws_security_group.idp_cleanup_users.id]
-  }
-
-  create_ecr_repository = false
-  billing_tag_value     = var.billing_tag_value
-}
-
 data "aws_iam_policy_document" "idp_cleanup_users_get_ssm_parameters" {
   statement {
     sid    = "GetSSMParameters"
@@ -53,30 +22,6 @@ resource "aws_ssm_parameter" "idp_cleanup_users_key_json" {
   tags  = local.core_tags
 }
 
-resource "aws_lambda_function_event_invoke_config" "idp_cleanup_users_event_invoke_config" {
-  function_name                = module.idp_cleanup_users_lambda.lambda_function_name
-  maximum_retry_attempts       = 2   # Maximum retry attempts for the Lambda function invocation
-  maximum_event_age_in_seconds = 300 # Maximum age of the event before it is discarded (in seconds)
-  destination_config {
-    on_failure {
-      destination = aws_sqs_queue.idp_event_cleanup_users_dlq_queue.arn
-    }
-  }
-}
-
-data "aws_iam_policy_document" "idp_cleanup_users_sqs" {
-  statement {
-    effect    = "Allow"
-    actions   = ["sqs:SendMessage"]
-    resources = [aws_sqs_queue.idp_event_cleanup_users_dlq_queue.arn]
-  }
-
-  statement {
-    effect    = "Allow"
-    actions   = ["kms:GenerateDataKey", "kms:Decrypt"]
-    resources = [aws_kms_key.sqs_dlq.arn]
-  }
-}
 resource "aws_sqs_queue" "idp_event_cleanup_users" {
   name                      = "idp-cleanup-users"
   kms_master_key_id         = aws_kms_key.sqs_dlq.arn
@@ -133,7 +78,7 @@ resource "aws_cloudwatch_event_target" "idp_cleanup_users_sqs" {
 module "idp_cleanup_users" {
   source = "github.com/cds-snc/terraform-modules//lambda?ref=v11.4.5"
 
-  name      = "idp-cleanup-users-lambda"
+  name      = "idp-cleanup-users"
   image_uri = "${aws_ecr_repository.repo["idp-cleanup-users"].repository_url}:latest"
   ecr_arn   = aws_ecr_repository.repo["idp-cleanup-users"].arn
 
