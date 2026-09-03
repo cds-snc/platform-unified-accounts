98c2cd36626a4e39e6b5875d6faf730b187beaf2
Omar Tehsin <omartehsin@gmail.com>
2026-09-03 10:07:27 -0400

    added lambda module

--- terraform/aws/idp_cleanup_users.tf
+++ terraform/aws/idp_cleanup_users.tf
@@ -129,3 +129,63 @@ resource "aws_cloudwatch_event_target" "idp_cleanup_users_sqs" {
   target_id = "idp-cleanup-users-sqs"
   arn       = aws_sqs_queue.idp_event_cleanup_users.arn
 }
+
+/*
+ * Lambda function that consumes cleanup-users events from the SQS event queue.
+ * Runs alongside the existing scheduled Lambda (idp_cleanup_users_lambda) for now;
+ * the CloudWatch event rule that feeds the queue is currently DISABLED.
+ */
+module "idp_cleanup_users_worker_lambda" {
+  source = "github.com/cds-snc/terraform-modules//lambda?ref=v11.4.5"
+
+  name      = "idp-cleanup-users-worker"
+  image_uri = "${aws_ecr_repository.repo["idp-cleanup-users"].repository_url}:latest"
+  ecr_arn   = aws_ecr_repository.repo["idp-cleanup-users"].arn
+
+  timeout       = 60
+  memory        = 1024
+  architectures = ["arm64"]
+
+  environment_variables = {
+    INACTIVE_DAYS                = 30
+    ZITADEL_PRIVATE_KEY_SSM_PATH = aws_ssm_parameter.idp_cleanup_users_key_json.name
+    ZITADEL_URL                  = "idp.${var.domain}"
+    DRY_RUN                      = "false"
+  }
+
+  vpc = {
+    subnet_ids         = module.idp_vpc.private_subnet_ids
+    security_group_ids = [aws_security_group.idp_cleanup_users.id]
+  }
+
+  policies = [
+    data.aws_iam_policy_document.idp_cleanup_users_get_ssm_parameters.json,
+    data.aws_iam_policy_document.idp_cleanup_users_worker_sqs.json
+  ]
+
+  billing_tag_value = var.billing_tag_value
+}
+
+data "aws_iam_policy_document" "idp_cleanup_users_worker_sqs" {
+  statement {
+    effect = "Allow"
+    actions = [
+      "sqs:ReceiveMessage",
+      "sqs:DeleteMessage",
+      "sqs:GetQueueAttributes",
+    ]
+    resources = [aws_sqs_queue.idp_event_cleanup_users.arn]
+  }
+
+  statement {
+    effect    = "Allow"
+    actions   = ["kms:Decrypt"]
+    resources = [aws_kms_key.sqs_dlq.arn]
+  }
+}
+
+resource "aws_lambda_event_source_mapping" "idp_cleanup_users_worker" {
+  event_source_arn = aws_sqs_queue.idp_event_cleanup_users.arn
+  function_name    = module.idp_cleanup_users_worker_lambda.function_name
+  batch_size       = 1
+}
