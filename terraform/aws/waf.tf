612b517298663cb8389125305a5e614fb2b71f30
mo-cds <mohamad.sbeiti@cds-snc.ca>
2026-09-10 11:42:28 -0400

    Created idp-internal WAF

--- terraform/aws/waf.tf
+++ terraform/aws/waf.tf
@@ -8,11 +8,11 @@ locals {
     "EC2MetaDataSSRF_BODY",          # Rule is blocking IdP OIDC app creation
     "EC2MetaDataSSRF_QUERYARGUMENTS" # Rule is blocking IdP OIDC login
   ]
-  rate_limit_all        = 1000
-  rate_limit_mutating   = 500
-  rate_limit_internal_all        = 4000
-  rate_limit_internal_mutating   = 2000
-  rate_limit_contact_us = 10
+  rate_limit_all               = 1000
+  rate_limit_mutating          = 500
+  rate_limit_internal_all      = 4000
+  rate_limit_internal_mutating = 2000
+  rate_limit_contact_us        = 10
 }
 
 resource "aws_wafv2_web_acl" "idp" {
